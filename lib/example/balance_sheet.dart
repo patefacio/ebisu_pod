b8ff8d0110a737475ccce288994cacf9eb6d14c3
Daniel Davidson <dbdavidson@yahoo.com>
2015-12-14 21:02:05 -0600

    fixed call to PodPackage

--- lib/example/balance_sheet.dart
+++ lib/example/balance_sheet.dart
@@ -10,49 +10,48 @@ final _logger = new Logger('balance_sheet');
 
 // custom <library balance_sheet>
 
-final balanceSheet = new PodPackage('balance_sheet')
-  ..namedTypes = [
-    enum_('holding_type', ['other', 'stock', 'bond', 'cash', 'blend',])
-      ..doc = '''
+final balanceSheet = new PodPackage('bs.balance_sheet', namedTypes: [
+  enum_('holding_type', ['other', 'stock', 'bond', 'cash', 'blend',])
+    ..doc = '''
 Is the holding stock (equity), bond, cash, some blend of those or other.''',
-    enum_('account_type', [
-      'other',
-      'roth_irs401k',
-      'traditional_irs401k',
-      'college_irs529',
-      'traditional_ira',
-      'investment',
-      'brokerage',
-      'checking',
-      'health_savings_account',
-      'savings',
-      'money_market',
-      'mattress',
-    ])..doc = 'Type of account',
-    object('date_value', [field('date', Date), field('value', Double)]),
-    object('holding', [
-      field('holding_type', 'holding_type')..doc = 'Type of the holding',
-      field('quantity', 'date_value')..doc = 'Quantity as of the date',
-      field('unit_value', 'date_value')..doc = 'Unit value as of the date',
-      field('cost_basis', Double)
-    ])
-      ..doc = '''
+  enum_('account_type', [
+    'other',
+    'roth_irs401k',
+    'traditional_irs401k',
+    'college_irs529',
+    'traditional_ira',
+    'investment',
+    'brokerage',
+    'checking',
+    'health_savings_account',
+    'savings',
+    'money_market',
+    'mattress',
+  ])..doc = 'Type of account',
+  object('date_value', [field('date', Date), field('value', Double)]),
+  object('holding', [
+    field('holding_type', 'holding_type')..doc = 'Type of the holding',
+    field('quantity', 'date_value')..doc = 'Quantity as of the date',
+    field('unit_value', 'date_value')..doc = 'Unit value as of the date',
+    field('cost_basis', Double),
+  ])
+    ..doc = '''
 The holding for a given symbol (or a sythetic aggregate as in an account other_holdings).
 
 Both quantity and unitValue have dates associated with them. The marketValue of
 the holding is based on the latest date of the two. This date can be different
 (most likely older) than the date associated with the BalanceSheet owning the
 holding.''',
-    object('portfolio_account')
-      ..doc =
-          'The map of holdings indexed by symbol (or similar name unique to the portfolio).'
-      ..fields = [
-        field('account_type', 'account_type')
-          ..doc = 'Type of the portfolio account',
-        field('descr')..doc = 'Description of the account',
-        field('stout', Boolean)..doc = 'Is it a beefy account',
-      ]
-  ];
+  object('portfolio_account')
+    ..doc =
+        'The map of holdings indexed by symbol (or similar name unique to the portfolio).'
+    ..fields = [
+      field('account_type', 'account_type')
+        ..doc = 'Type of the portfolio account',
+      field('descr')..doc = 'Description of the account',
+      field('stout', Boolean)..doc = 'Is it a beefy account',
+    ]
+]);
 
 // end <library balance_sheet>
 
